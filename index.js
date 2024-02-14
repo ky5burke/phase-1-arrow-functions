51b9b461dee636abc5c51874bbc6fa47e85bcb40
ky5burke <ky5burke@gmail.com>
2024-02-13 19:06:35 -0600

    arrow functions

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const divide = parameter => parameter / 100;
+
+divide(2000);
+
+const square = parameter => parameter**2;
+
+const add = (parameterOne, parameterTwo) => parameterOne + parameterTwo;
+
